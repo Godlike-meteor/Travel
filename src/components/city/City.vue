86383b6466ca31a17641eb3e2dcb546a5470784a
Godlike Meteor <1814533740@qq.com>
2019-01-27 00:04:37 +0800

    city-router git push

---
+++ src/components/city/City.vue
@@ -0,0 +1,18 @@
+<!-- 城市选择页 -->
+<template>
+  <city-header></city-header>
+</template>
+
+<script>
+import CityHeader from './components/Header'
+export default {
+  name: 'City',
+  components: {
+    CityHeader
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+
+</style>
