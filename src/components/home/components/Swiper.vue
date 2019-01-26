657f8a1b78bc593615be605addf2d857a61e117f
Godlike Meteor <1814533740@qq.com>
2019-01-26 10:55:30 +0800

    add awiper

---
+++ src/components/home/components/Swiper.vue
@@ -0,0 +1,47 @@
+<!-- 首页-Swiper -->
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl">
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home-Swiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true,
+        autoplay: 3000,
+        autoplayDisableOnInteraction: false
+      },
+      swiperList: [{
+        id: '001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1811/de/f141c53c57475f02.jpg_750x200_4ca8370f.jpg'
+      }, {
+        id: '002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1812/d6/daa880b254940402.jpg_750x200_b114308a.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background-color: #fff !important
+  .wrapper
+    overflow: hidden
+    height: 0
+    width: 100%
+    padding-bottom: 26.5%
+    background-color: #eee
+    .swiper-img
+      width: 100%
+</style>
