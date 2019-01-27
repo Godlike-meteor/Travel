b6028f1a0d793b5315ab4daa2297a1d709d5207c
Godlike Meteor <1814533740@qq.com>
2019-01-27 14:47:05 +0800

    add city-ajax

--- src/components/city/components/Alphabet.vue
+++ src/components/city/components/Alphabet.vue
@@ -1,17 +1,16 @@
 <!-- 城市选择页 -->
 <template>
   <ul class="list">
-    <li class="item">A</li>
-    <li class="item">B</li>
-    <li class="item">C</li>
-    <li class="item">D</li>
-    <li class="item">E</li>
+    <li class="item" v-for="(item, key) of cities" :key="key">{{key}}</li>
   </ul>
 </template>
 
 <script>
 export default {
-  name: 'City-Alphabet'
+  name: 'City-Alphabet',
+  props: {
+    cities: Object
+  }
 }
 </script>
 
