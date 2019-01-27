4a816d9d067d0837c70df46458c9b555c63254df
Godlike Meteor <1814533740@qq.com>
2019-01-27 12:43:38 +0800

    add Search

---
+++ src/components/city/components/Search.vue
@@ -0,0 +1,29 @@
+<!-- 城市选择页 -->
+<template>
+  <div class="search">
+    <input class="search-input" type="text" placeholder="输入城市名或拼音" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'City-Search'
+}
+</script>
+
+<style lang='stylus' scoped>
+  @import '~styles/varibles.styl'
+  .search
+    height .72rem
+    background-color $bgColor
+    padding 0 .1rem
+    .search-input
+      box-sizing border-box
+      height .62rem
+      line-height .62rem
+      padding 0 .1rem
+      width 100%
+      text-align center
+      border-radius .02rem
+      color #666
+</style>
