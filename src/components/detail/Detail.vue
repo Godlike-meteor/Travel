be1310fc4eb9c2a53fde687004465a4432f4590e
Godlike Meteor <1814533740@qq.com>
2019-01-28 11:54:06 +0800

    ajax git push

--- src/components/detail/Detail.vue
+++ src/components/detail/Detail.vue
@@ -1,7 +1,7 @@
 <!-- 详情页 -->
 <template>
   <div>
-    <detail-banner></detail-banner>
+    <detail-banner :bannerImgs="gallaryImgs" :sightName="sightName" :bannerImg="bannerImg"></detail-banner>
     <detail-header></detail-header>
     <div class="content">
       <detail-list :list="list"></detail-list>
@@ -13,6 +13,7 @@
 import detailBanner from './compoments/Banner'
 import detailHeader from './compoments/Header'
 import detailList from './compoments/List'
+import axios from 'axios'
 export default {
   name: 'Detail',
   components: {
@@ -22,24 +23,33 @@ export default {
   },
   data () {
     return {
-      list: [{
-        title: '成人票',
-        children: [{
-          title: '成人三馆联票',
-          children: [{
-            title: '成人三馆联票 - 特惠'
-          }]
-        }, {
-          title: '成人五馆联票'
-        }]
-      }, {
-        title: '学生票'
-      }, {
-        title: '儿童票'
-      }, {
-        title: '特惠票'
-      }]
+      sightName: '',
+      bannerImg: '',
+      gallaryImgs: [],
+      list: []
     }
+  },
+  methods: {
+    getDetailInfo () {
+      axios.get('/api/detail.json', {
+        params: {
+          id: this.$route.params.id
+        }
+      }).then(this.handleGetDataSucc)
+    },
+    handleGetDataSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.sightName = data.sightName
+        this.bannerImg = data.bannerImg
+        this.gallaryImgs = data.gallaryImgs
+        this.list = data.categoryList
+      }
+    }
+  },
+  mounted () {
+    this.getDetailInfo()
   }
 }
 </script>
