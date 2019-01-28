142c08da9ef51c4eeedbb662cef43009a22b2f38
Godlike Meteor <1814533740@qq.com>
2019-01-28 10:53:26 +0800

    detail-list add

--- src/components/detail/Detail.vue
+++ src/components/detail/Detail.vue
@@ -3,18 +3,43 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
-    <div class="content"></div>
+    <div class="content">
+      <detail-list :list="list"></detail-list>
+    </div>
   </div>
 </template>
 
 <script>
 import detailBanner from './compoments/Banner'
 import detailHeader from './compoments/Header'
+import detailList from './compoments/List'
 export default {
   name: 'Detail',
   components: {
     detailBanner,
-    detailHeader
+    detailHeader,
+    detailList
+  },
+  data () {
+    return {
+      list: [{
+        title: '成人票',
+        children: [{
+          title: '成人三馆联票',
+          children: [{
+            title: '成人三馆联票 - 特惠'
+          }]
+        }, {
+          title: '成人五馆联票'
+        }]
+      }, {
+        title: '学生票'
+      }, {
+        title: '儿童票'
+      }, {
+        title: '特惠票'
+      }]
+    }
   }
 }
 </script>
