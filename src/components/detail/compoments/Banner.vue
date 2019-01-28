c760fdc47b07e123fb2ab7071a940583856c645d
Godlike Meteor <1814533740@qq.com>
2019-01-28 12:39:56 +0800

    animation add

--- src/components/detail/compoments/Banner.vue
+++ src/components/detail/compoments/Banner.vue
@@ -11,12 +11,15 @@
         </div>
       </div>
     </div>
-    <common-gallary :imgs="bannerImgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>
+    <fade-animation>
+      <common-gallary :imgs="bannerImgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>
+    </fade-animation>
   </div>
 </template>
 
 <script>
 import CommonGallary from 'common/gallary/Gallary'
+import FadeAnimation from 'common/fade/FadeAnimation'
 export default {
   name: 'Detail-Banner',
   props: {
@@ -38,7 +41,8 @@ export default {
     }
   },
   components: {
-    CommonGallary
+    CommonGallary,
+    FadeAnimation
   }
 }
 </script>
